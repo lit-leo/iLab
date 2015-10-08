90b353ee294a72d11a5ea4fa536dc7ed6725d8fa
Litleo97 <litleo@mail.ru>
2015-10-08 22:14:49 +0300

    Normal output with structures

--- Task 1/Quad_roots_v.0.2.c
+++ Task 1/Quad_roots_v.0.2.c
@@ -1,48 +1,65 @@
 #include <stdio.h>
 #include <math.h>
 
-double Lin_root ( double b , double c ){
+#define DISCRIMINANT(x,y,z) (y*y - 4*x*z)
+#define HI_ROOT(a,b,c) ( -b + sqrt (DISCRIMINANT(a,b,c)) )/(2*a)
+#define LO_ROOT(a,b,c) ( -b - sqrt (DISCRIMINANT(a,b,c)) )/(2*a)
+
+struct root {
+    double fir;
+    double sec;
+};
+
+double Lin_root(double b ,double c );
+
+struct root Quad_root(double a, double b, double c){
+    struct root temp;
+    temp.fir = 0;
+    temp.sec = 0;
+
+    if (Roots_count(a,b,c) == -1)
+    {
+        temp.fir = Lin_root(b,c);
+        return temp;
+    };
+    if (Roots_count(a,b,c) == 0)
+    {
+        printf("No real roots.");
+        return temp;
+    };
+    if (Roots_count(a,b,c) == 1)
+    {
+        temp.fir = HI_ROOT(a,b,c);
+        temp.sec = HI_ROOT(a,b,c);
+        return temp;
+    };
+    if (Roots_count(a,b,c) == 2)
+    {
+        temp.fir = HI_ROOT(a,b,c);
+        temp.sec = LO_ROOT(a,b,c);
+        return temp;
+    };
+};
+
+double Lin_root(double b ,double c ){
 
     double root;
 
     if ( ( b == 0 ) && ( c == 0 ) ) {
         printf("Quantity of roots are infinite");
+        return 0;
     }
     else {
         if ( b == 0 ) {
             printf("No roots \n");
+            return 0;
         }
 
         else {
             root = - c / b ;
             printf("%lg\n", root);
-
-        };
-    };
-}
-
-double Quad_root_hi ( double a , double b , double c) { //calculates a value of a higher root and returns its value if possible
-     if ( Roots_count( a , b , c) >= 1 ) {
-    double root =( - b + sqrt ( b * b - 4 * a *c) )/ ( 2 * a );
-    return root ;
-     }
-     else {
-        if ( Roots_count( a , b , c) == 0 ) {
-                printf("No real roots");
-            }
-        else {
-            printf("Expected quadratic equalization, met linear\n");
+            return root;
         }
-     }
-}
-
-double Quad_root_low ( double a , double b , double c) { //calculates a value of a lower root and returns its value if possible
-    if ( Roots_count( a , b , c) >= 1) {
-        double root = ( - b - sqrt ( b * b - 4 * a *c) )/ ( 2 * a );
-        return root ;
-    }
-    else {
-        return ;
     }
 }
 
@@ -69,16 +86,16 @@ int Roots_count( double a , double b , double c) { //counts quantity of roots, r
     };
 }
 
-double Quadratic_roots_display ( double a , double b , double c) { //shows on display roots of equalization, if it has ones
+/*double Quadratic_roots_display ( double a , double b , double c) { //shows on display roots of equalization, if it has ones
 
     double Discriminant = ( b * b ) - (4 * a * c);
 
-    if ( Roots_count( a , b , c) == 0 ) {
-        printf("No real roots");
-    };
-
     if ( Roots_count( a , b , c) == -1 ) {
         return Lin_root ( b , c );
+    };
+
+    if ( Roots_count( a , b , c) == 0 ) {
+        printf("No real roots");
     };
 
     if ( Roots_count( a , b , c) == 1 ) {
@@ -92,13 +109,15 @@ double Quadratic_roots_display ( double a , double b , double c) { //shows on di
     return 0;
 
 }
-
+*/
 int main() {
     double a , b , c ;
     int d;
 
     if ( ( d = scanf("%lg %lg %lg", &a,&b,&c) ) == 3 ) {
-        Quadratic_roots_display ( a, b, c);
+        //Quadratic_roots_display(a,b,c);
+        printf("\n%lg\n",Quad_root(a,b,c).fir);
+        printf("\n%lg\n",Quad_root(a,b,c).sec);
     }
     else {
         printf("Unexpected input data\n");
