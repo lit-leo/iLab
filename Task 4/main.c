f8aed4172079ae252ebf128b3f31daf7cd585458
Leonid <litleo@mail.ru>
2015-10-29 20:13:30 +0300

    Better codestyle

--- Task 4/main.c
+++ Task 4/main.c
@@ -5,23 +5,23 @@
 int main()
 {
     stack_s a;
+    
     double k;
     ctor(&a, 1, 10);
     stack_dump(a);
-    
     push(&a, 2);
     dump(a);
     push(&a, 4);
     dump(a);
-    //scanf("%lg", &k);
-    //push(&a, k);
+    scanf("%lg", &k);
+    push(&a, k);
     pop(&a);
     dump(a);
     pop(&a);
-    //pop(&a);
     dump(a);
-    printf("%d\n", isEmpty(a));
+    printf("\nIsEmpty : %d\n", isEmpty(a));
     dtor(&a);
+    
     return 0;
 }
 
